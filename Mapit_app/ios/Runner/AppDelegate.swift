d799b8bb4e1d1d0da6fbf87d5cb41b872d5b5fab
MateoZouko <mateozouko1@gmail.com>
2024-03-11 15:33:55 -0300

    adding ios

--- Mapit_app/ios/Runner/AppDelegate.swift
+++ Mapit_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBYS2ViGdRWGPZvaqFeeVBIOQeGvLitXJ0") // Aquí reemplaza "tu_clave_de_api_de_google_maps" con tu clave de API
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
